3b534cc86aebfa66ceb429c94bc703bf78129f5e
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2025-10-20 14:08:21 +0100

    switch to storage account id

--- containers.tf
+++ containers.tf
@@ -1,7 +1,7 @@
 
 resource "azurerm_storage_container" "container" {
   for_each              = { for container in var.containers : container.name => container }
-  storage_account_name  = azurerm_storage_account.storage_account.name
+  storage_account_id    = azurerm_storage_account.storage_account.id
   name                  = each.value.name
   container_access_type = each.value.access_type
 }
